18544c3e054eb8e2ab5ac73aadec91da20bafe45
menkeler <53800362+menkeler@users.noreply.github.com>
2023-12-01 13:35:52 +0800

    save models

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin, UserManager
 
-# Create your models here.
+
+class CustomUserManager(UserManager):
+    def _create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('An email is required.')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email, password, **extra_fields)
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    user_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=200, unique=True)
+    contact = models.CharField(max_length=15, default=None)
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(default='user.png')
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        ordering = ['-date_joined']
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
